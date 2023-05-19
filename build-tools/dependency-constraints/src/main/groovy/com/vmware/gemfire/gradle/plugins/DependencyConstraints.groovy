8d4db30d28f370810b8b03ac83d59ad1a66c2309
Udo Kohlmeyer <udo@vmare.com>
2023-05-19 21:02:16 +0200

    Final commit with updated GA versions for Spring Data and Boot 3.1.0 and Springframework 6.0.9

--- build-tools/dependency-constraints/src/main/groovy/com/vmware/gemfire/gradle/plugins/DependencyConstraints.groovy
+++ build-tools/dependency-constraints/src/main/groovy/com/vmware/gemfire/gradle/plugins/DependencyConstraints.groovy
@@ -23,10 +23,10 @@ class DependencyConstraints {
     depVersionMapping.put("antlrVersion", "2.7.7")
     depVersionMapping.put("findbugsVersion", "3.0.2")
     depVersionMapping.put("multithreadedtcVersion", "1.01")
-    depVersionMapping.put("springBootVersion", "3.1.0-RC1")
+    depVersionMapping.put("springBootVersion", "3.1.0")
     depVersionMapping.put("springShellVersion", "1.2.0.RELEASE")
-    depVersionMapping.put("springFrameworkVersion", "6.0.8")
-    depVersionMapping.put("springSecurityVersion", "6.1.0-RC1")
+    depVersionMapping.put("springFrameworkVersion", "6.0.9")
+    depVersionMapping.put("springSecurityVersion", "6.1.0")
     depVersionMapping.put("testcontainersVersion", "1.17.6")
 
     return depVersionMapping
