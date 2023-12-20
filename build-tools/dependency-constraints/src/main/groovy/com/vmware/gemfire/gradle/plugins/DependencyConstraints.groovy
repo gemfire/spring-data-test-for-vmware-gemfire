2ac8ce2f6d013ac215de4c22d7d8989c1da2e177
Patrick Johnson <jpatrick@vmware.com>
2023-12-20 10:53:19 -0800

    SDGF-184: update patches

--- build-tools/dependency-constraints/src/main/groovy/com/vmware/gemfire/gradle/plugins/DependencyConstraints.groovy
+++ build-tools/dependency-constraints/src/main/groovy/com/vmware/gemfire/gradle/plugins/DependencyConstraints.groovy
@@ -23,10 +23,10 @@ class DependencyConstraints {
     depVersionMapping.put("antlrVersion", "2.7.7")
     depVersionMapping.put("findbugsVersion", "3.0.2")
     depVersionMapping.put("multithreadedtcVersion", "1.01")
-    depVersionMapping.put("springBootVersion", "3.1.4")
+    depVersionMapping.put("springBootVersion", "3.1.6")
     depVersionMapping.put("springShellVersion", "1.2.0.RELEASE")
-    depVersionMapping.put("springFrameworkVersion", "6.0.12")
-    depVersionMapping.put("springSecurityVersion", "6.1.4")
+    depVersionMapping.put("springFrameworkVersion", "6.0.15")
+    depVersionMapping.put("springSecurityVersion", "6.1.6")
     depVersionMapping.put("testcontainersVersion", "1.17.6")
 
     return depVersionMapping
