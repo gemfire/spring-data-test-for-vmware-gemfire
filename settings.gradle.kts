c454065ace8c54ae2d01c6b929f0f697c2d3b8a0
Patrick Johnson <jpatrick@vmware.com>
2024-01-05 16:22:24 -0800

    SDGF-192: Convert Gradle to Kotlin and introduce version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+import java.io.FileInputStream
+import java.util.*
+
+pluginManagement {
+  includeBuild("build-tools/publishing")
+}
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "spring-test-for-vmware-gemfire"
+
+include("spring-test-gemfire")
+
+dependencyResolutionManagement {
+  versionCatalogs {
+    create("libs") {
+      val properties = Properties()
+      properties.load(FileInputStream("gradle.properties"))
+      versionOverrideFromProperties(this, properties)
+    }
+  }
+}
+
+private fun versionOverrideFromProperty(versionCatalogBuilder: VersionCatalogBuilder, propertyName: String, propertiesFile: Properties): String {
+  val propertyValue = providers.systemProperty(propertyName).getOrElse(propertiesFile.getProperty(propertyName))
+  return versionCatalogBuilder.version(propertyName, propertyValue)
+}
+
+private fun versionOverrideFromProperties(versionCatalogBuilder: VersionCatalogBuilder, properties: Properties) {
+  versionOverrideFromProperty(versionCatalogBuilder, "gemfireVersion", properties)
+  versionOverrideFromProperty(versionCatalogBuilder, "springDataGemFireVersion", properties)
+}
