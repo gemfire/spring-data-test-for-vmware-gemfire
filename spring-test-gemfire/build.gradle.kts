716f4d96d73604bc64b46522f05824ccb82aa0a5
Udo Kohlmeyer <udo@vmware.com>
2024-02-03 12:18:58 +1100

    Update versions before release Fix version patch logic

--- spring-test-gemfire/build.gradle.kts
+++ spring-test-gemfire/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 }
 
 repositories {
-  val additionalMavenRepoURLs = project.ext.get("additionalMavenRepoURLs") as String
+  val additionalMavenRepoURLs:String by project
   if (additionalMavenRepoURLs.isNotEmpty() && additionalMavenRepoURLs.isNotBlank()) {
     additionalMavenRepoURLs.split(",").forEach {
       project.repositories.maven {
