94e7a00a698f9d673e042b95c7474d95131f19c4
Udo Kohlmeyer <udo@vmware.com>
2024-03-21 10:39:00 +1100

    SDGF-240: Update copyright

--- spring-test-gemfire/src/main/shell/deleteGeodeFiles.sh
+++ spring-test-gemfire/src/main/shell/deleteGeodeFiles.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#
+# Copyright 2024 Broadcom. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
 rm -Rf `find . -name "BACKUPDEFAULT*"`
 rm -Rf `find . -name "ConfigDiskDir*"`
 rm -Rf `find . -name "locator*" | grep -v "src"`
